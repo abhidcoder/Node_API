3b299e64935db5edaca00b959d6439aac1fc330f
abhidcoder <74507289+abhidcoder@users.noreply.github.com>
2023-10-22 21:16:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const app = express();                          // Create a router instead of an
 const cors = require('cors');                   //Enabled CORS (Cross-Origin Resource Sharing) for your server.
 const postRoutes = require('./routes/post');    // Import the routes from the post file
 const postSamples = require('./routes/importSampleData');  // Import the routes from the importSampleData file
-const postImages = require('./firebase/uploadScript'); //
+const postImages = require('./firebase/uploadScript'); // Import the routes from the uploadScript file
 require('dotenv').config();                     //Loaded environment variables from a .env file using dotenv.
 
-app.use(cors());
-app.use(express.json());                        //Used express.json() to enable JSON parsing for incoming requests.
+app.use(cors());                                // applying cross resource sharing
+app.use(express.json());                        // Used express.json() to enable JSON parsing for incoming requests.
 app.use('/v1', postRoutes);                     // Using the '/post' prefix for the POST API
 app.use('/insert', postSamples);                // Using the '/insert' prefix for the POST datas from the json
-app.use('/bucket', postImages);                 // Using the '/bucket' prefix for the add image in the bucker
+app.use('/bucket', postImages);                 // Using the '/bucket' prefix for the add file in the Firebase bucket
 
 
-//Handled the root route with a response to check if the server is working.
+//Handled the root route with a response to checks if the server is working.
 app.get("/", async (req, res) => {
   res.send( 'Node Server is Running.')
 } )
